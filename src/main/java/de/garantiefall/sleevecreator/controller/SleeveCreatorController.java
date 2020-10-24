153d0dd741dc7bf2ae73b98a6ff1058305062c93
Garantiefall <garantiefall@t-online.de>
2020-10-24 23:42:15 +0200

    Sleeve Creator

--- src/main/java/de/garantiefall/sleevecreator/controller/SleeveCreatorController.java
+++ src/main/java/de/garantiefall/sleevecreator/controller/SleeveCreatorController.java
@@ -4,30 +4,35 @@ import java.io.IOException;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import de.garantiefall.sleevecreator.entity.FileEntity;
 
 @Controller
 public class SleeveCreatorController {
-	@Autowired
-	private FileEntity fileEntity; 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SleeveCreatorController.class);
+
 	
+	public static final String SESSION_KEY = "fileToEdit";
 	@GetMapping("/sleevecreator")
 	public String showSleevecreator() {
+		System.out.println("SleeveCreatorController.showSleevecreator()");
 		return "sleevecreator";
 	}
 	
-	@PostMapping("/sleevecreator/upload")
-	public String showSleevecreator(@RequestParam("file") MultipartFile file) throws IOException {
+	@PostMapping("/sleevecreator")
+	public String showSleevecreator(@RequestParam(name = "fileSelector") MultipartFile file, HttpSession session) throws IOException {
+		LOGGER.info("file: " + file);
 		if(file != null && file.getBytes() != null) {
 			FileEntity fileEntity = new FileEntity();
 			fileEntity.setId(UUID.randomUUID().toString());
@@ -35,21 +40,30 @@ public class SleeveCreatorController {
 			fileEntity.setType(file.getContentType());
 			fileEntity.setData(file.getBytes());
 			
+			session.setAttribute(SESSION_KEY, fileEntity);
+			LOGGER.info("File in Session!");
 			return "redirect:/sleevecreator/create";
 		}
 		return "redirect:/sleevecreator";
 	}
 	
 	@GetMapping("/sleevecreator/create")
-	public String showSleeveCreatorSite(Model model) {
-		return "sleevecratorEditor";
+	public String showSleeveCreatorSite(Model model, HttpSession session) {
+		if(session.getAttribute(SESSION_KEY) != null) {
+			model.addAttribute("fileToCreate", (FileEntity) session.getAttribute(SESSION_KEY));
+			return "sleevecreatorEditor";
+		}
+		return "redirect:/sleevecreator";
 	}
 	
-	@GetMapping("/sleevecreator/getFile")
-	public byte[] getFile(@PathVariable("id") String id, HttpServletResponse response) {
-		if(fileEntity.getData() != null && fileEntity.getData().length > 0) {
-			response.addHeader("Content-Type", fileEntity.getType());
-			return fileEntity.getData();
+	@ResponseBody
+	@GetMapping("/sleevecreator/create/getFile")
+	public byte[] getFile(HttpServletResponse response, HttpSession session) {
+		FileEntity file = (FileEntity) session.getAttribute(SESSION_KEY);
+		LOGGER.info("File Content getFile: " + file);
+		if(file != null) {
+			response.addHeader("Content-Type", file.getType());
+			return file.getData();
 		}
 		return null;
 	}
--- src/main/java/de/garantiefall/sleevecreator/entity/FileEntity.java
+++ src/main/java/de/garantiefall/sleevecreator/entity/FileEntity.java
@@ -1,10 +1,5 @@
 package de.garantiefall.sleevecreator.entity;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Component
-@Scope("session")
 public class FileEntity {
 	private String id;
 	private String filename;
